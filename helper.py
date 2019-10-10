c57d7505b43719cc1223a6b348220a50bfd73e0d
shwetanshusingh2 <32520526+shwetanshusingh2@users.noreply.github.com>
2019-10-10 13:22:08 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -2,36 +2,38 @@ import boto3
 from botocore.client import ClientError
 import time
 import utils as u
+import parameters as p
 import os
 
 
-os.environ['AWS_SHARED_CREDENTIALS_FILE'] = "C:/Users/Hp/.aws/credentials"
 
-os.environ['AWS_PROFILE'] = "default"
-os.environ['AWS_DEFAULT_REGION'] = "ap-south-1"
+os.environ['AWS_SHARED_CREDENTIALS_FILE'] = p.credential_name
+
+os.environ['AWS_PROFILE'] = p.credential_name
+os.environ['AWS_DEFAULT_REGION'] = p.location
 
 s3 = boto3.resource('s3')
 try:
-    s3.create_bucket(Bucket='data-shwet', CreateBucketConfiguration={'LocationConstraint': 'ap-south-1'})
+    s3.create_bucket(Bucket=p.data_bucket, CreateBucketConfiguration={'LocationConstraint': p.location})
 except ClientError:
     print("Data Bucket Already Created")
-u.upload_object('data-shwet', 'template.yaml', 'template.yaml')
-u.upload_zip_object('data-shwet', 'lambda_function.py', 'lambda_function.zip', 'lambda_function.zip')
+u.upload_object(p.data_bucket, p.template_name, p.template_name)
+u.upload_zip_object(p.data_bucket, 'lambda_function.py', 'lambda_function.zip', 'lambda_function.zip')
 client = boto3.client('cloudformation')
-status = u.stack_status('stack')
+status = u.stack_status(p.stack_name)
 if status == 'ROLLBACK_COMPLETE' or status == 'ROLLBACK_FAILED' or status == 'UPDATE_ROLLBACK_COMPLETE' or status == \
         'DELETE_FAILED':
-    u.delete_object('shwet23')
-    u.delete_object('shwet23-des')
-    client.delete_stack(StackName='stack')
+    u.delete_object(p.names["source_bucket"])
+    u.delete_object(p.names["dest_bucket"])
+    client.delete_stack(StackName=p.stack_name)
     while u.stack_status('stack') == 'DELETE_IN_PROGRESS':
         time.sleep(1)
     print("stack deleted")
-    u.create_stack('stack', 'https://data-shwet.s3.ap-south-1.amazonaws.com/template.yaml', 'shwet23', 'shwet23-des')
+    u.create_stack(p.stack_name, p.names["template_url"], p.names["source_bucket"], p.names["dest_bucket"])
     print("stack created")
 elif status == 'CREATE_COMPLETE' or status == 'UPDATE_COMPLETE' :
-    u.update_stack('stack', 'https://data-shwet.s3.ap-south-1.amazonaws.com/template.yaml', 'shwet23', 'shwet23-des')
+    u.update_stack(p.stack_name, p.names["template_url"], p.names["source_bucket"], p.names["dest_bucket"])
     print("stack updated")
 else:
-    u.create_stack('stack', 'https://data-shwet.s3.ap-south-1.amazonaws.com/template.yaml', 'shwet23', 'shwet23-des')
+    u.create_stack(p.stack_name, p.names["template_url"], p.names["source_bucket"], p.names["dest_bucket"])
     print("stack created")
