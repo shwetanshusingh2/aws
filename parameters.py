f80519d54531ce06eeba9a4174b503cbb3fee9b2
shwetanshusingh2 <32520526+shwetanshusingh2@users.noreply.github.com>
2019-10-10 13:23:56 +0530

    Add files via upload

---
+++ parameters.py
@@ -0,0 +1,8 @@
+
+names = {"source_bucket":"shwet23" , "dest_bucket":"shwet23-des","template_url":'https://data-shwet.s3.ap-south-1.amazonaws.com/template.yaml',"stack_name":"stack"}
+credential_path="C:/Users/Hp/.aws/credentials"
+credential_name="default"
+location="ap-south-1"
+data_bucket='data-shwet'
+template_name='template.yaml'
+stack_name="stack"
